16afc4be17a0c93505a36d7df41c42cd6953ae8f
aldrin163 <aldrinjenson@gmail.com>
2020-04-23 21:08:55 +0530

    add html and css loaders. before building.

--- webpack.config.js
+++ webpack.config.js
@@ -4,16 +4,40 @@
 // basically, consider webPack as the coordinator and see the helper as the one who actually transpiles the code to the reqiored version
 // here apparently, babel is the helper library which transpiles jsx into older javascrip
 
+const htmlWebpackPlugin = require('html-webpack-plugin')
+
 module.exports = {
-  module:{
-    rules:[
+  module: {
+    rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use:{
+        use: {
+          loader: "babel-loader", // this is gonna be lookin in the root directory for a file called babel rc
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"], // evaluated from right to left
+        // css loader is used first
+      },
+      {
+        test: /\.html$/,
+        use: {
+          loader: "html-loader",
+        },
+      },
+    ],
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+      template: "./index.html",
+    }),
+  ],
+};
 
-        }
-      }
-    ]
-  }
-}
+// We are installing
+// babel core,
+// babel preset-env specifically for transpiling es6 to es5
+// babel preset react specifically for react
+// babel loader just for the babel loader for webpack
